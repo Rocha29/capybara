bd880df0df63f0f176bc15dd082a2bc1526c32e0
rocha <inforobinho@gmail.com>
2020-01-20 23:40:25 -0300

    adicioando combobox select e autocomplete

---
+++ features/step_definitions/combobox_dropdown_autocomplete.rb
@@ -0,0 +1,15 @@
+Quando("interajo com dropdown e select") do
+    visit '/buscaelementos/dropdowneselect'
+    find('.btn.dropdown-button').click
+    find('#dropdown3').click
+    select 'Chrome', from:'dropdown'
+    find('option[value="2"]').select_option
+    
+  end
+  
+  Quando("preencho o autocomplete") do
+    visit '/widgets/autocomplete'
+    find('#autocomplete-input').set 'Rio de Janeiro'
+    find('ul', text: 'Rio de Janeiro').click
+   
+  end
